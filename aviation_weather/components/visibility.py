2b830fa391f38cd4ca9a560e1f9ac232b359d00d
Stephen James <stephenorjames@gmail.com>
2016-05-09 01:25:11 -0400

    Added support for CAVOK

--- aviation_weather/components/visibility.py
+++ aviation_weather/components/visibility.py
@@ -17,45 +17,56 @@ class Visibility(Component):
             VisibilityDecodeError: If `raw` could not be parsed.
         """
         m = re.search(
-            r"\b(?:(?P<lt_gt>[MP])?(?P<distance1>\d[ \d/]{,4})(?P<unit>SM)|(?P=lt_gt)?(?P<distance2>\d{4}))\b",
+            r"\b(?:(?P<lt_gt>[MP])?(?P<distance1>\d[ \d/]{,4})(?P<unit>SM)|(?P=lt_gt)?(?P<distance2>\d{4})|(?P<cavok>CAVOK))\b",
             raw
         )
         if not m:
             raise VisibilityDecodeError("Visibility(%r) could not be parsed" % raw)
-        self.less_than = m.group("lt_gt") == "M"
-        self.greater_than = m.group("lt_gt") == "P"
-        distance = m.group("distance1") or m.group("distance2")
-        self.unit = m.group("unit") or "m"
-
-        if "/" in distance:
-            parts = distance.split(" ", 1)
-            if len(parts) == 1:
-                self.distance = float(Fraction(parts[0]))
-            else:
-                self.distance = int(parts[0]) + float(Fraction(parts[1]))
+        if m.group("cavok"):
+            self.cavok = True
+            self.distance = None
+            self.greater_than = False
+            self.less_than = False
+            self.unit = None
         else:
-            self.distance = int(distance)
+            self.cavok = None
+            self.less_than = m.group("lt_gt") == "M"
+            self.greater_than = m.group("lt_gt") == "P"
+            distance = m.group("distance1") or m.group("distance2")
+            self.unit = m.group("unit") or "m"
+
+            if "/" in distance:
+                parts = distance.split(" ", 1)
+                if len(parts) == 1:
+                    self.distance = float(Fraction(parts[0]))
+                else:
+                    self.distance = int(parts[0]) + float(Fraction(parts[1]))
+            else:
+                self.distance = int(distance)
 
     @property
     def raw(self):
-        if self.less_than:
-            raw = "M"
-        elif self.greater_than:
-            raw = "P"
+        if self.cavok:
+            return "CAVOK"
         else:
-            raw = ""
+            if self.less_than:
+                raw = "M"
+            elif self.greater_than:
+                raw = "P"
+            else:
+                raw = ""
 
-        whole = self.distance // 1
-        fraction = Fraction(self.distance % 1)
-        if whole and fraction:
-            raw += "%d %s" % (whole, fraction)
-        elif whole:
-            raw += str(whole)
-        elif fraction:
-            raw += str(fraction)
-        else:
-            raw += "0"
+            whole = self.distance // 1
+            fraction = Fraction(self.distance % 1)
+            if whole and fraction:
+                raw += "%d %s" % (whole, fraction)
+            elif whole:
+                raw += str(whole)
+            elif fraction:
+                raw += str(fraction)
+            else:
+                raw += "0"
 
-        if self.unit != "m":
-            raw += self.unit
-        return raw
+            if self.unit != "m":
+                raw += self.unit
+            return raw
--- tests/components/test_visibility.py
+++ tests/components/test_visibility.py
@@ -6,37 +6,41 @@ from aviation_weather.exceptions import VisibilityDecodeError
 class TestVisibility(unittest.TestCase):
     """Unit tests for aviation_weather.components.visibility.Visibility"""
 
-    def _test_valid(self, raw, less_than, greater_than, distance, unit):
+    def _test_valid(self, raw, cavok, less_than, greater_than, distance, unit):
         v = Visibility(raw)
         self.assertEqual(raw, v.raw)
+        self.assertEqual(cavok, v.cavok)
         self.assertEqual(less_than, v.less_than)
         self.assertEqual(greater_than, v.greater_than)
         self.assertEqual(distance, v.distance)
         self.assertEqual(unit, v.unit)
 
     def test_valid_1_SM(self):
-        self._test_valid("1SM", False, False, 1, "SM")
+        self._test_valid("1SM", None, False, False, 1, "SM")
 
     def test_valid_20_SM(self):
-        self._test_valid("20SM", False, False, 20, "SM")
+        self._test_valid("20SM", None, False, False, 20, "SM")
 
     def test_valid_fraction_SM(self):
-        self._test_valid("1/2SM", False, False, .5, "SM")
+        self._test_valid("1/2SM", None, False, False, .5, "SM")
 
     def test_valid_less_fraction_SM(self):
-        self._test_valid("M1/2SM", True, False, .5, "SM")
+        self._test_valid("M1/2SM", None, True, False, .5, "SM")
 
     def test_valid_less_mixed_SM(self):
-        self._test_valid("M1 1/4SM", True, False, 1.25, "SM")
+        self._test_valid("M1 1/4SM", None, True, False, 1.25, "SM")
 
     def test_valid_greater_SM(self):
-        self._test_valid("P6SM", False, True, 6, "SM")
+        self._test_valid("P6SM", None, False, True, 6, "SM")
 
     def test_valid_9999_m(self):
-        self._test_valid("9999", False, False, 9999, "m")
+        self._test_valid("9999", None, False, False, 9999, "m")
 
     def test_valid_2000_m(self):
-        self._test_valid("2000", False, False, 2000, "m")
+        self._test_valid("2000", None, False, False, 2000, "m")
+
+    def test_valid_cavok(self):
+        self._test_valid("CAVOK", True, False, False, None, None)
 
     def _test_invalid(self, raw):
         with self.assertRaises(VisibilityDecodeError):

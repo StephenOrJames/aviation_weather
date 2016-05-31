2ef54d5f41fcea29702bcafd9ff80602536649e9
Stephen James <stephenorjames@gmail.com>
2016-05-31 22:23:55 +0000

    Implemented parsing remarks for AO

--- aviation_weather/components/remarks.py
+++ aviation_weather/components/remarks.py
@@ -1,3 +1,5 @@
+import re
+
 from aviation_weather.components import Component
 from aviation_weather.exceptions import RemarksDecodeError
 
@@ -9,18 +11,25 @@ class Remarks(Component):
 
         Args:
             raw (str): The remarks to be stored (not actually parsed at the moment).
+            ao (int): The type of automated station. If AO1, then 1, or if AO2, then 2. Otherwise, the value is None.
 
         Raises:
             RemarksDecodeError: If `raw` could not be parsed.
         """
-        if raw.startswith("RMK "):
-            self.text = raw
-        else:
+        if not raw.startswith("RMK "):
             raise RemarksDecodeError("Remarks(%r) could not be parsed" % raw)
+
+        self.text = raw
+
+        ao = re.search(r"\bAO(?P<ao>[12])\b", raw)
+        if ao:
+            self.ao = int(ao.group("ao"))
+        else:
+            self.ao = None
 
     @property
     def decoded(self):
-        return ""  # We technically don't parse remarks.
+        return ""  # We don't fully parse remarks.
 
     @property
     def raw(self):
--- tests/components/test_remarks.py
+++ tests/components/test_remarks.py
@@ -6,12 +6,51 @@ from aviation_weather.exceptions import RemarksDecodeError
 class TestRemarks(unittest.TestCase):
     """Unit tests for aviation_weather.components.remarks.Remarks"""
 
-    def test_valid(self):
-        s = "RMK AO2 PK WND 14027/1802 SLP183 P0018 T01170106"
-        r = Remarks(s)
-        self.assertEqual(s, r.raw)
-        self.assertEqual(s, r.text)
+    def _test_valid(self, raw, ao):
+        rmk = Remarks(raw)
+        self.assertEqual(raw, rmk.raw)
+        self.assertEqual(raw, rmk.text)
+        self.assertEqual(ao, rmk.ao)
 
-    def test_invalid(self):
+    def test_valid_1(self):
+        self._test_valid(
+            raw="RMK AO2 PK WND 14027/1802 SLP183 P0018 T01170106",
+            ao=2
+        )
+
+    def test_valid_2(self):
+        self._test_valid(
+            raw="RMK AO2 RAB06E37 SLP203 SHRA DSNT W P0024 60026 T02440222 50005",
+            ao=2
+        )
+
+    def test_valid_3(self):
+        self._test_valid(
+            raw="RMK AO1",
+            ao=1
+        )
+
+    def test_valid_4(self):
+        self._test_valid(
+            raw="RMK AO1 WEA:-RA KH P0001",
+            ao=1
+        )
+
+    def test_valid_5(self):
+        self._test_valid(
+            raw="RMK 8/200 HZY MSGTX MMMD",
+            ao=None
+        )
+
+    def _test_invalid(self, raw):
         with self.assertRaises(RemarksDecodeError):
-            Remarks("AO2 PK WND 14027/1802 SLP183 P0018 T01170106")
+            Remarks(raw)
+
+    def test_invalid_1(self):
+        self._test_invalid("")
+
+    def test_invalid_2(self):
+        self._test_invalid("AO2 PK WND 14027/1802 SLP183 P0018 T01170106")
+
+    def test_invalid_3(self):
+        self._test_invalid("This RMK is not valid")
